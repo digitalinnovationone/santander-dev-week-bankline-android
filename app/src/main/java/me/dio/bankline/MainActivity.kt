36755c3ef4ad4e5e5e1f32040a321814ef9347ed
Venilton FalvoJr <falvojr@gmail.com>
2022-04-29 17:29:28 -0300

    first commit

---
+++ app/src/main/java/me/dio/bankline/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.dio.bankline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
